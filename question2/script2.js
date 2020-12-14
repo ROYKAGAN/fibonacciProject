d48a01449953d80ec90c6f0cbf53b5545f59f4f9
ROY KAGAN <roiekagan8@gmail.com>
2020-12-14 09:13:51 +0200

    change name to index

--- question2/script2.js
+++ question2/script2.js
@@ -2,7 +2,7 @@ function checkInput(){
     var password = document.getElementById("sample").value;
     console.log(password);
     if (password == 123){
-        document.createElement("a").text = "to pass to the next level"
+        window.location.pathname = "/path/to/another/file.html/";
     }
 
     else{
