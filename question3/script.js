22b3d7909e157df563ff882b1698957098fac157
ROYKAGAN <roiekagan8@gmail.com>
2020-12-20 13:45:47 +0200

    sladh

--- question3/script.js
+++ question3/script.js
@@ -1,17 +1,25 @@
-function checkInput(){
+function goToPass(){
+    window.location.pathname = "/fibonacciProject/question3/password.html";
+}
 
+function backToQ(){
+    window.location.pathname = "/fibonacciProject/question3/index3.html";
+}
+
+function checkInput(){
+    
     var password = document.getElementById("sample").value;
 
-    if (password == 340){
+    if (password == 810){
         var head = document.getElementsByTagName('HEAD')[0];  
         var link = document.createElement('link'); 
 
         link.rel = 'stylesheet';
         link.href = 'correct.css';
-        
-        head.appendChild(link);
 
         alert("כל הכבוד")
+
+        head.appendChild(link);
         //next  q:
         window.location.pathname = "/fibonacciProject/question7/index.html";
     }
@@ -29,10 +37,3 @@ function checkInput(){
     document.getElementById("sample").value = ""
 }
 
-function goToPass(){
-    window.location.pathname = "/fibonacciProject/question3/password.html";
-}
-
-function backToQ(){
-    window.location.pathname = "/fibonacciProject/question3/index3.html";
-}
